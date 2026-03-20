202907ab14cb42ead670a7540d50356909a81a54
MaritoSebastian <maritosebastianchavez1975@gmai.com>
2026-03-20 11:24:17 -0300

    se corrige nav por /Nav para que funcione el despliegue en vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/navbar/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 import Banner from "./components/Banner/Banner";
 import "./App.css";
 import Footer from "./components/Footer/Footer";
