a47b1cd724f332504cc652aa1951bae0aa09c1cf
MaritoSebastian <maritosebastianchavez1975@gmai.com>
2026-09-23 11:34:56 -0300

    agrego logs en el success para ver que devuelve mp

--- src/components/Pages/PaymentStatus/Success.jsx
+++ src/components/Pages/PaymentStatus/Success.jsx
@@ -6,9 +6,23 @@ const Success = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+
+  console.log("=== RESPUESTA MERCADO PAGO ===");
+  console.log("URL COMPLETA:", window.location.href);
+  console.log("collection_id:", params.get("collection_id"));
+  console.log("collection_status:", params.get("collection_status"));
+  console.log("payment_id:", params.get("payment_id"));
+  console.log("status:", params.get("status"));
+  console.log("external_reference:", params.get("external_reference"));
+  console.log("merchant_order_id:", params.get("merchant_order_id"));
+  console.log("==============================");
+
+
     const timer = setTimeout(() => {
+      
       navigate("/"); 
-    }, 4000);
+    }, 10000);
 
     return () => clearTimeout(timer);
   }, [navigate]);
